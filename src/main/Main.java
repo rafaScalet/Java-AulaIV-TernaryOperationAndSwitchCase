34ea8a833830c6a2123ddb6cc0d3829388807b1d
rafaScalet <rafaelscalet2018@gmail.com>
2024-04-20 13:50:22 -0300

    ternary operator example

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,7 +1,32 @@
 package main;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        
+        // <condição seletora> ? <valor de retorno verdadeiro> : <valor de retorno falso>
+        int a, b, aux;
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Enter the numbers: ");
+        a = in.nextInt();
+        b = in.nextInt();
+
+        in.close();
+
+        aux = a;
+        a = (a > b) ? b : a;
+        b = (a == b) ? aux : b;
+
+        /*
+        this is the same thing
+        if (a > b) {
+            aux = a;
+            a = b;
+            b = aux;
+        }
+        */
+
+        System.out.println("Result: " + a + ", " + b);
     }
 }
