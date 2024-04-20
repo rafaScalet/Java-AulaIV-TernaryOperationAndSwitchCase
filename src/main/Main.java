1bcfdd8800bb05ae1b0f59067e8d9b2668cf20c3
rafaScalet <rafaelscalet2018@gmail.com>
2024-04-20 14:12:40 -0300

    While and Do While loop example

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,47 +1,20 @@
 package main;
 
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
-        int number;
-        Scanner in = new Scanner(System.in);
+        int number = 0;
 
-        System.out.print("Enter the number: ");
-        number = in.nextInt();
-        in.close();
-
-        switch (number) {
-            case 1:
-                System.out.println("Categoria " + number);
-                break;
-            case 2:
-                System.out.println("Categoria " + number);
-                break;
-            case 3:
-                System.out.println("Categoria " + number);
-                break;
-            default:
-                break;
+        while(number < 10){
+            System.out.println("[number]" + number);
+            number++;
         }
-
 
         /*
         is the same thing
-        switch (number) {
-            case 1:
-            case 2:
-            case 3:
-                System.out.println("Categoria " + number);
-                break;
-            default:
-                break;
-        }
-
-        and this too
-        if(number == 1 || number == 2 || number == 3){
-            System.out.println("Categoria " + number);
-        }
+        do{
+            System.out.println("[number]" + number);
+            number++;
+        } while(number < 10);
         */
     }
 }
